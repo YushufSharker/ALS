e95abd4114686d345696feb3f3a6c809e51d9e8d
YushufSharker <yushuf.sharker@takeda.com>
2025-02-10 11:50:58 -0500

    Another update

--- lognormal_mean_variance.R
+++ lognormal_mean_variance.R
@@ -27,6 +27,8 @@
 #' varx <- 29.5
 #' convert_to_lognormal(meanx, varx)
 #'
+#'Check the finding with SAS version privided in
+#'https://blogs.sas.com/content/iml/2014/06/04/simulate-lognormal-data-with-specified-mean-and-variance.html
 
 convert_to_lognormal <- function(meanx, varx) {
   varlog <- round(log(varx / meanx ^ 2 + 1), 4)
